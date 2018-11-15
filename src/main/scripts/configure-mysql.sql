9f2064464f0db437c05511ac8e9afa69b33d0171
risswyck <risswyck@gmx.de>
2018-11-15 09:32:57 +0100

    mysql dev and prod db

--- src/main/scripts/configure-mysql.sql
+++ src/main/scripts/configure-mysql.sql
@@ -5,28 +5,28 @@ CREATE DATABASE sfg_dev;
 CREATE DATABASE sfg_prod;
 
 # users
-CREATE USER 'sfg_dev'@'localhost' IDENTIFIED BY 'guru';
-CREATE USER 'sfg_prod'@'localhost' IDENTIFIED BY 'guru';
-CREATE USER 'sfg_dev'@'%' IDENTIFIED BY 'guru';
-CREATE USER 'sfg_prod'@'%' IDENTIFIED BY 'guru';
+CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'guru';
+CREATE USER 'sfg_dev_prod'@'localhost' IDENTIFIED BY 'guru';
+CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY 'guru';
+CREATE USER 'sfg_dev_prod'@'%' IDENTIFIED BY 'guru';
 
 # permissions
-GRANT SELECT ON sfg_dev.* TO 'sfg_dev'@'localhost';
-GRANT INSERT ON sfg_dev.* TO 'sfg_dev'@'localhost';
-GRANT UPDATE ON sfg_dev.* TO 'sfg_dev'@'localhost';
-GRANT DELETE ON sfg_dev.* TO 'sfg_dev'@'localhost';
+GRANT SELECT ON sfg_dev.* TO 'sfg_dev_user'@'localhost';
+GRANT INSERT ON sfg_dev.* TO 'sfg_dev_user'@'localhost';
+GRANT UPDATE ON sfg_dev.* TO 'sfg_dev_user'@'localhost';
+GRANT DELETE ON sfg_dev.* TO 'sfg_dev_user'@'localhost';
 
-GRANT SELECT ON sfg_prod.* TO 'sfg_prod'@'localhost';
-GRANT INSERT ON sfg_prod.* TO 'sfg_prod'@'localhost';
-GRANT UPDATE ON sfg_prod.* TO 'sfg_prod'@'localhost';
-GRANT DELETE ON sfg_prod.* TO 'sfg_prod'@'localhost';
+GRANT SELECT ON sfg_prod.* TO 'sfg_dev_prod'@'localhost';
+GRANT INSERT ON sfg_prod.* TO 'sfg_dev_prod'@'localhost';
+GRANT UPDATE ON sfg_prod.* TO 'sfg_dev_prod'@'localhost';
+GRANT DELETE ON sfg_prod.* TO 'sfg_dev_prod'@'localhost';
 
-GRANT SELECT ON sfg_dev.* TO 'sfg_dev'@'%';
-GRANT INSERT ON sfg_dev.* TO 'sfg_dev'@'%';
-GRANT UPDATE ON sfg_dev.* TO 'sfg_dev'@'%';
-GRANT DELETE ON sfg_dev.* TO 'sfg_dev'@'%';
+GRANT SELECT ON sfg_dev.* TO 'sfg_dev_user'@'%';
+GRANT INSERT ON sfg_dev.* TO 'sfg_dev_user'@'%';
+GRANT UPDATE ON sfg_dev.* TO 'sfg_dev_user'@'%';
+GRANT DELETE ON sfg_dev.* TO 'sfg_dev_user'@'%';
 
-GRANT SELECT ON sfg_prod.* TO 'sfg_prod'@'%';
-GRANT INSERT ON sfg_prod.* TO 'sfg_prod'@'%';
-GRANT UPDATE ON sfg_prod.* TO 'sfg_prod'@'%';
-GRANT DELETE ON sfg_prod.* TO 'sfg_prod'@'%';
+GRANT SELECT ON sfg_prod.* TO 'sfg_dev_prod'@'%';
+GRANT INSERT ON sfg_prod.* TO 'sfg_dev_prod'@'%';
+GRANT UPDATE ON sfg_prod.* TO 'sfg_dev_prod'@'%';
+GRANT DELETE ON sfg_prod.* TO 'sfg_dev_prod'@'%';
